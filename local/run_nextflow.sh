295f8bf866a4679b07f3e31a8efe84b9f8f562cc
Haglund <ah3918@ic.ac.uk>
2024-12-04 14:42:15 +0000

    restructure

---
+++ local/run_nextflow.sh
@@ -0,0 +1,8 @@
+
+cd /Users/ah3918/Dropbox/LANDMARK/PROJECTS/eQTL_PIPELINE
+
+nextflow run -c /Users/ah3918/Dropbox/LANDMARK/PROJECTS/eQTL_PIPELINE/NEXTFLOW/nextflow_local.config \
+/Users/ah3918/Dropbox/LANDMARK/PROJECTS/eQTL_PIPELINE/NEXTFLOW/eqtl_pipeline.nf -profile offline \
+--gds_file /Users/ah3918/Dropbox/LANDMARK/PROJECTS/eQTL_PIPELINE/data/inputs/test_geno.gds \
+--single_cell_file /Users/ah3918/Dropbox/LANDMARK/PROJECTS/eQTL_PIPELINE/data/inputs/roche_ms_decontx.rds \
+--outdir /Users/ah3918/Dropbox/LANDMARK/PROJECTS/eQTL_PIPELINE/data/outputs
