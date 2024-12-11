a57f8b7a4330044638f2458d181bdefd209416b2
Haglund <ah3918@ic.ac.uk>
2024-12-11 14:55:28 +0000

    adding few more params

--- R/MatrixEQTL_functions/matrixeqtl_source.r
+++ R/MatrixEQTL_functions/matrixeqtl_source.r
@@ -91,7 +91,7 @@ library(MatrixEQTL)
     # Calculate PCs from the expression matrix
     pca <- prcomp(exp_mat, scale. = TRUE, center = TRUE)
     pcs <- pca$rotation
-    max_pcs <- min(floor(ncol(pcs) / 10) * 10, n_indivs - 1)
+    max_pcs <- ncol(pcs)-10
     pcs <- pcs[, 1:max_pcs]
     pcs <- t(pcs)
     pcs <- pcs[, colnames(exp_mat)]
