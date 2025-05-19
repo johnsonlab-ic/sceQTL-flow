3d756e890c9d35ee1f10cb79565b3925fadcd15c
HaglundA <ah3918@ic.ac.uk>
2025-05-19 08:48:07 +0000

    added docker images to github registry

--- Images/setup_registry.sh
+++ Images/setup_registry.sh
@@ -9,21 +9,21 @@ echo $CR_PAT | docker login ghcr.io -u HaglundA --password-stdin
 # Format: ghcr.io/USERNAME/IMAGE_NAME:TAG
 
 # Build Expression image
-docker build -t ghcr.io/haglunda/eqtl-expression:latest -f Images/Dockerfile.expression .
+docker build -t ghcr.io/johnsonlab-ic/eqtl-expression:latest -f Images/Dockerfile.expression .
 
 # Build Genotype image
-docker build -t ghcr.io/haglunda/eqtl-genotype:latest -f Images/Dockerfile.genotype .
+docker build -t ghcr.io/johnsonlab-ic/eqtl-genotype:latest -f Images/Dockerfile.genotype .
 
 # Build Python image
-docker build -t ghcr.io/haglunda/eqtl-python:latest -f Images/Dockerfile.python .
+docker build -t ghcr.io/johnsonlab-ic/eqtl-python:latest -f Images/Dockerfile.python .
 
 # Build Reports image
-docker build -t ghcr.io/haglunda/eqtl-reports:latest -f Images/Dockerfile.reports .
+docker build -t ghcr.io/johnsonlab-ic/eqtl-reports:latest -f Images/Dockerfile.reports .
 
 # Push images to GitHub Container Registry
-docker push ghcr.io/haglunda/eqtl-expression:latest
-docker push ghcr.io/haglunda/eqtl-genotype:latest
-docker push ghcr.io/haglunda/eqtl-python:latest
-docker push ghcr.io/haglunda/eqtl-reports:latest
+docker push ghcr.io/johnsonlab-ic/eqtl-expression:latest
+docker push ghcr.io/johnsonlab-ic/eqtl-genotype:latest
+docker push ghcr.io/johnsonlab-ic/eqtl-python:latest
+docker push ghcr.io/johnsonlab-ic/eqtl-reports:latest
 
 echo "All images have been built and pushed to GitHub Container Registry!"
